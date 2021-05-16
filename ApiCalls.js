6fdfe21a9996a8b4c58dc78c120ece28e391ff86
Akshay Chauhan <52008891+akshaychauhan-ac@users.noreply.github.com>
2021-05-16 14:02:29 +0530

    Update and rename ApiCalls.txt to ApiCalls.js

--- ApiCalls.js
+++ ApiCalls.js
@@ -1,39 +1,37 @@
-1) XMLHttpRequest
+// 1) XMLHttpRequest
 
 let request = new XMLHttpRequest();
 request.open("GET", "https://jsonplaceholder.typicode.com/users");
 request.send();
-
 // request.status
 // request.response
 
 
-2) Fetch
+// 2) Fetch
 
 fetch("https://jsonplaceholder.typicode.com/users")
 	.then(res => res.json())
 	.then(users => console.log(users));
 
-Async/Await
+// Async/Await
 
 async function getUsers() {
 	let response = await fetch("https://jsonplaceholder.typicode.com/users");
 	return response.json();
 }
-
 getUsers().then(data => console.log(data));
 
-3) Axios
 
-npm i axios
+// 3) Axios
+
+// npm i axios
 
 axios.get("https://jsonplaceholder.typicode.com/users")
 	.then(response => console.log(data))
 	.catch(err => console.error(err));
 
-4) Ajax Api call by jQuery
-
-"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+// 4) Ajax Api call by jQuery
+	// "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
 
 $.ajax({
   url: "https://jsonplaceholder.typicode.com/users",
@@ -45,8 +43,3 @@ $.ajax({
     console.log(error);
   }
 });
-
-
-
-
-Async/Await

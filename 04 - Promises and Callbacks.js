9af0bdf213ae65400d510d2cbe1e87a95c30dda9
Akshay <akshay.chauhan@sap.com>
2021-05-17 19:06:43 +0530

    .

---
+++ 04 - Promises and Callbacks.js
@@ -0,0 +1,44 @@
+// Promises and Callbacks (async/await)
+
+// A promise is an object that may produce a single value some time in the future: either a resolved value, or a reason that it’s not resolved (e.g., a network error occurred). 
+// A promise may be in one of 3 possible states: fulfilled, rejected, or pending. Promise users can attach callbacks to handle the fulfilled value or the reason for rejection.
+//		Fulfilled: onFulfilled() will be called (e.g., resolve() was called)
+//		Rejected: onRejected() will be called (e.g., reject() was called)
+//		Pending: not yet fulfilled or rejected
+
+var promise = new Promise(function(resolve, reject) {
+  const x = "geeksforgeeks";
+  const y = "geeksforgeeks"
+  if(x === y) {
+    resolve();
+  } else {
+    reject();
+  }
+});
+  
+promise
+	.then(function () {
+	    console.log('Success, You are a GEEK');
+	}).
+	catch(function () {
+	    console.log('Some error has occured');
+	});
+
+// Geeks For Geeks
+
+/******************************************************************************************************/
+
+var promise = new Promise(function(resolve, reject) {
+    throw new Error('Some error has occured')
+})
+  
+promise
+	.then(function(successMessage) {
+	    console.log(successMessage);
+	})
+	.catch(function(errorMessage) {
+	   //error handler function is invoked
+	    console.log(errorMessage);
+	});
+
+// Error: Some error has occured

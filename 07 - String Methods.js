9269856342ad3c061f3aba8b0d7855f85e358729
Akshay <akshay.chauhan@sap.com>
2021-06-08 11:00:24 +0530

    .

---
+++ 07 - String Methods.js
@@ -0,0 +1,48 @@
+/************************************************** String Methods *****************************************************/
+
+// 1) charAt - Returns character at index
+      
+      let str = "string";
+
+      str.charAt(3);
+      // output - "i"
+
+// 2) concat - Concatenates multiple strings provided to method
+
+      const str1 = 'Hello';
+      const str2 = 'World';
+
+      str1.concat(' ', str2);
+      // output: "Hello World"
+
+// 3) includes - case-sensitive search & returns boolean
+
+      const sentence = 'The quick brown fox jumps over the lazy dog.';
+      const word = 'fox';
+
+      sentence.includes(word);
+      // output: true
+
+// 4) indexOf
+
+// 5) lastIndexOf
+
+// 6) replace
+
+// 7) replaceAll
+
+// 8) search
+
+// 9) slice
+
+// 10) split
+
+// 11) substring
+
+// 12) toLowerCase
+
+// 13) toString
+
+// 14) toUpperCase
+
+// 15) trim
